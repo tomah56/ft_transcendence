efc5f25ebfdaa6afc94f7ddfe1fb556a23dde3ed
Didar Oreshev <doreshev@student.42heilbronn.de>
2023-03-28 11:54:51 +0200

    some front added

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,13 +1,27 @@
-import PingPong from "./game/Game"
-import Chat from "./chat/chat"
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import GameView from "./game/GameView"
+
 import React from 'react'
+import Auth from "./auth/Auth";
+import {BrowserRouter as Router, Link} from "react-router-dom";
+import Button from "@mui/material/Button/Button";
+import PingPong from "./game/Game";
+import GameView from "./game/GameView";
+import Chat from "./chat/Chat";
 
 export default function App() {
+    const isAuthorised = Validator();
+
+    if (isAuthorised == false)
+        return (
+            <div className="App">
+                <Auth/>
+            </div>
+        )
     return (
         <div className="App">
             <Router>
+                <header>
+                    <Link to={"Chat"} > <Button variant="outlined">Authentification</Button></Link>
+                </header>
                 <Routes>
                     <Route path="/" element={<PingPong/>}/>
                     <Route path="/GameView" element={<GameView/>}/>
@@ -15,5 +29,5 @@ export default function App() {
                 </Routes>
             </Router>
         </div>
-    );
+    )
 }
