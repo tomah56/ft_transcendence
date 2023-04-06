b7840dc46e211d0344e052e65dde0e933f33989c
Didar Oreshev <doreshev@student.42heilbronn.de>
2023-04-06 19:09:43 +0200

    before merging with main

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -27,13 +27,13 @@ export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 
     constructor(private readonly gameService: GameService) {}
 
-    @SubscribeMessage('gameUpdate')
+    @SubscribeMessage('update')
     gameUpdate(@MessageBody() dto: GameDataDto,
                @ConnectedSocket() client: Socket) {
         if (this.gameService.isPlayer(client.id)) {
             const gameId = this.gameService.getGameId(client.id);
             if (gameId)
-                this.server.to(String(gameId)).emit('gameUpdate', dto);
+                this.server.to(String(gameId)).emit('update', dto);
         }
     }
 
@@ -45,7 +45,7 @@ export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
         if (this.gameService.isPlayer(client.id)) {
             const gameId = this.gameService.getGameId(client.id);
             if (this.gameService.isStarted(gameId))
-                this.server.to(String(gameId)).emit("playerDisconnected", gameId);
+                this.server.to(String(gameId)).emit("disconnect", gameId);
             else {
                 this.gameService.deletePlayer(client.id);
                 this.gameService.deleteGame(gameId);
