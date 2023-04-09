06e4eb0cd1e19502b24adc0ab625c72ed6a5cb16
Didar Oreshev <doreshev@student.42heilbronn.de>
2023-04-09 14:25:19 +0200

    local

--- frontend/src/components/basic.tsx
+++ frontend/src/components/basic.tsx
@@ -12,7 +12,7 @@ export default function Basic() {
     return (
         <>
             <section>
-                 <CreateGame/>
+                 <PingPong/>
             </section>
         </>
     );
