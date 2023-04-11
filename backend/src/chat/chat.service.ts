7c155d8f0104b4556eb581a9450ac522c4496a64
Didar Oreshev <doreshev@student.42heilbronn.de>
2023-04-11 12:10:45 +0200

    fixed default value for entity and not alloving it to be nullable to prevent errors

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -33,11 +33,6 @@ export class ChatService {
             throw new HttpException('Wrong data provided!', HttpStatus.BAD_REQUEST);
         const chat = this.chatRepository.create(dto);
         chat.owner = owner.id;
-        chat.admins = [];
-        chat.bannedUsers = [];
-        // chat.mutedUsers = new Array<MutedUser>(); //todo find solution for this
-        chat.users = [];
-        chat.messages = [];
         chat.users.push(owner.id);
         await this.chatRepository.save(chat);
         await this.userServices.addChat(owner, chat.id);
