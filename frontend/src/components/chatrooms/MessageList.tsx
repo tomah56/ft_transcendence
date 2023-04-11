2c034af508c63b098b77a81716bf76296eaf099b
Tamas Tokesi <t.tomah@hotmail.com>
2023-04-11 17:29:36 +0200

    massage view css update

--- frontend/src/components/chatrooms/MessageList.tsx
+++ frontend/src/components/chatrooms/MessageList.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 // import Message from './Message';
 import './chatstart.css';
+import axios from "axios";
+import {User} from "../BaseInterface";
+
 
 
 // interfaces user {
@@ -15,9 +18,39 @@ import './chatstart.css';
 //   text: string;
 // }
 
+interface ChatProps {
+user : User;
+chatidp: string;
+chatName : string;
+}
 
-export default function MessageList() {
-  const [messages, setMessages] = useState([]);
+const MessageList: React.FC<ChatProps> = (props : ChatProps) => {
+
+// export default function MessageList() {
+const [messages, setMessages] = useState<{content: string, chat: string, date: string, id: string, displayName: string, user: string, type :string }[]>([]);;
+const [msg, setmsg] = useState([]);
+
+
+useEffect(() => {
+// console.log("massage log");
+async function printmessages() {
+	await axios.get("http://localhost:5000/chat/messages/" + props.chatidp, {withCredentials: true})
+	.then( response => {
+		setMessages(response.data);
+		// console.log("printmassage");
+		// console.log(response.data);
+	})
+	.catch((reason) => {
+		if (reason.response!.status !== 200) {
+		console.log("Error getting massages");
+		console.log(props.chatidp);
+	}
+	console.log(reason.message);
+	});
+}
+printmessages();
+}, [messages.length]);
+
 //   useEffect(() => {
 //   getmassagedata() => {
 //           const data = doc.data();
@@ -33,28 +66,34 @@ export default function MessageList() {
 //       });
 //   }, [ messages.length ]);
 
-  return (
-    <>
-    </>
-    // <div id="message-list">
-    //   {messages.map((message) => {
-    //     return (
-    //       <div className={ `message ${user.displayName !== message.sender ? 'message-reverse' : ''}` }>
-    //       <div className="datetime">
-    //         <div className="datetime-date">{message.date}</div>
-    //         <div className="datetime-time">{message.time}</div>
-    //       </div>
-    //       <div className="sender">
-    //         <div className="sender-image">
-    //           <img src={message.photoURL} alt={message.sender} title={message.sender} />
-    //         </div>
-    //         <div className="sender-name d-none">{message.sender}</div>
-    //       </div>
-    //       <div className="text">{message.text}</div>
-    //     </div>
-    //         // <Message key={message.id} message={message} user={props.user} />
-    //     );
-    //   })}
-    // </div>
-  );
+return (
+<>
+	<div id="message-list">
+	{messages.map((message) => {
+		return (
+			<div className={ `message ${props.user.displayName !== message.displayName ? 'message-reverse' : ''}` }>
+				{/* <div className='aligningbox'> */}
+
+					<div className="datetime">
+						<div className="datetime-date">{message.date}</div>
+					</div>
+					<div className="sender">
+						{/* <div className="sender-image">
+							<img src={message.photoURL} alt={message.displayName} title={message.displayName} />
+						</div> */}
+						<div className="sender-name d-none">{message.displayName}</div>
+					</div>
+						<div className="chattext">{message.content}</div>
+				{/* </div> */}
+			 </div>
+			// <Message key={message.id} message={message} user={props.user} />
+		);
+	})}
+</div>
+
+</>
+
+);
 }
+
+export default MessageList;
--- frontend/src/components/chatrooms/NewChat.tsx
+++ frontend/src/components/chatrooms/NewChat.tsx
@@ -3,6 +3,7 @@ import astroman from '../img/littleman.png';
 import axios from "axios";
 import Chat from "../chat/Chat"
 import {User} from "../BaseInterface";
+import MessageList from './MessageList';
 
 interface ChatProps {
     user : User;
@@ -17,16 +18,16 @@ interface ChatProps {
     const [msg, setmsg] = useState([]);
     // const [chatId, setchatId] = useState(0 || chatidp); //set with basic value 0
 
-    useEffect(() => {
-        console.log("massage log");
-        async function printmessages() {
-            const response = await axios.get("http://localhost:5000/chat/id/" + props.chatidp, {withCredentials: true});
-            setmsg(response.data);
-            console.log("printmassage");
-            console.log(response.data);
-        }
-        printmessages();
-    }, []);
+    // useEffect(() => {
+    //     // console.log("massage log");
+    //     async function printmessages() {
+    //         const response = await axios.get("http://localhost:5000/chat/id/" + props.chatidp, {withCredentials: true});
+    //         setmsg(response.data);
+    //         console.log("printmassage");
+    //         console.log(response.data);
+    //     }
+    //     printmessages();
+    // }, []);
 
     function handOnClickSend() {
         let temp = "Anonymus";
@@ -37,6 +38,7 @@ interface ChatProps {
         <>
             <div className='formholder'>
                 <p>{props.chatName}</p>
+                <MessageList user={props.user} chatidp={props.chatidp} chatName={props.chatName}/>
                 <Chat  user={props.user} chatidp={props.chatidp} />
             </div>
         </>
