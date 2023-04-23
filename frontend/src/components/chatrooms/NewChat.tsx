ecc7aa1bbabaedb6fb361fdacbd85c9e2395606d
Tamas Tokesi <t.tomah@hotmail.com>
2023-04-23 15:20:20 +0200

    adding user functionality

--- frontend/src/components/chatrooms/NewChat.tsx
+++ frontend/src/components/chatrooms/NewChat.tsx
@@ -8,14 +8,17 @@ import InputMessage from './InputMessage';
 import { ChatSocketProvider } from '../context/chat-socket';
 
 interface ChatProps {
-user : User;
-chatidp: string;
-chatName : string;
+	user : User;
+	chatidp: string;
+	updatestate: number;
+	chatName : string;
 }
 
 const NewChat: React.FC<ChatProps> = (props : ChatProps) => {
 const [usersData, setUsersData] = useState<any[]>([]);
+const [chatData, setchatData] = useState<{users: any[]}>();
 
+const [addThisUser, setaddThisUser] = useState<string>("");
 const [title, setTitle] = useState('');
 const [urlpost, setUrlpost] = useState('');
 const [bigtext, setBigtext] = useState('');
@@ -23,16 +26,20 @@ const [msg, setmsg] = useState([]);
 
 // const [chatId, setchatId] = useState(0 || chatidp); //set with basic value 0
 
-// useEffect(() => {
-//     // console.log("massage log");
-//     async function printmessages() {
-//         const response = await axios.get("http://localhost:5000/chat/id/" + props.chatidp, {withCredentials: true});
-//         setmsg(response.data);
-//         console.log("printmassage");
-//         console.log(response.data);
-//     }
-//     printmessages();
-// }, []);
+useEffect(() => {
+    // console.log("massage log");
+    async function getChatData() {
+        await axios.get("http://localhost:5000/chat/id/" + props.chatidp, {withCredentials: true})
+		.then((response) => {
+			setchatData(response.data);
+			// setmsg(response.data);
+			// console.log("chat data");
+			// console.log(response.data);
+
+		});
+    }
+    getChatData();
+}, [props.chatidp]);
 
 useEffect(() => {
 	axios.get(`http://${window.location.hostname}:5000/users`, { withCredentials: true })
@@ -45,11 +52,31 @@ useEffect(() => {
 				console.log("error in getting all user data");
 			}
 		});
-	}, [props.chatidp]);
+	}, [props.chatidp, props.updatestate]);
 
 useEffect(() => {
 	console.log(usersData);
-}, [props.chatidp]);
+}, [props.chatidp, props.updatestate]);
+
+useEffect(() => {
+	if (addThisUser !== "")
+	{
+		axios.post(`http://${window.location.hostname}:5000/chat/addUser`,  { userId : addThisUser,  chatId : props.chatidp }, {withCredentials: true})
+		.then( () => {
+			// setaddThisUser(""); //do i need this?
+		}).catch((reason) => {
+			if (reason.response!.status !== 200) {
+				console.log("Error while adding user:");
+				console.log(addThisUser);
+				console.log("in chatid:");
+				console.log(props.chatidp);
+			}
+			console.log(reason.message);
+		});
+	}
+}, [addThisUser]);
+
+
 
 function addUserHandler(UserId :string) {
 	axios.post(`http://${window.location.hostname}:5000/chat/addUser`,  { userId : UserId,  chatId : props.chatidp }, {withCredentials: true}).then( () => {
@@ -82,11 +109,19 @@ return (
 					<span>
 						{props.chatName} chat:
 					</span>
+					{usersData && usersData.map((item, index) => (
+							<div key={index} className='' style={{color: "white"}}>
+								{item.id !== props.user.id 
+									&& chatData 
+									&&	!(chatData.users.includes(item.id)) 
+									&& <button className='' onClick={() => {
+										setaddThisUser(item.id);
+									}} >add {item.displayName}</button>}
+							</div>
+						))}
 				</div>
-					{/* <MessageList user={props.user} chatidp={props.chatidp} chatName={props.chatName} chatContent={parentState} /> */}
 			<ChatSocketProvider>	
 				<InputMessage user={props.user} chatidp={props.chatidp} chatName={props.chatName} onUpdate={handleParentStateUpdate}/>
-				{/* <Chat  user={props.user} chatidp={props.chatidp} /> */}
 			</ChatSocketProvider>
 		</div>
 	</>
